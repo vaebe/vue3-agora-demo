f8191fd142b098c8b0474cfd52e0cbd345072e00
vaebe <18137693952@163.com>
2024-04-19 21:27:50 +0800

    feat: 设置项目配置信息

--- src/views/layout/baseLayout.vue
+++ src/views/layout/baseLayout.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import {defineAsyncComponent} from 'vue'
 
+const LayoutHeader = defineAsyncComponent(() => import('./components/Header.vue'))
 </script>
 
 <template>
-<div>
+<div class="base-layout">
+  <layout-header></layout-header>
   <router-view></router-view>
 </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.base-layout{
+  width: 100vw;
+  height: 100vh;
+  background: #e5e5e5;
+}
 </style>
---
+++ src/views/layout/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {defineAsyncComponent} from 'vue'
+
+const SetProjectConfig = defineAsyncComponent(() => import('./SetProjectConfig.vue'))
+
+</script>
+
+<template>
+<div class="layout-header">
+  dasdasda
+  <set-project-config></set-project-config>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.layout-header{
+  position: relative;
+  width: 100vw;
+  height: 68px;
+  background: #f5f5f5;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    filter: blur(10px);
+    z-index: -1;
+  }
+}
+</style>
