32868eee88176eb09df865320b613fa9c5c801ec
vaebe <18137693952@163.com>
2024-05-03 23:35:03 +0800

    feat: github 部署基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '/' : '/vue3-agora-demo/',
   plugins: [
     vue(),
     AutoImport({
