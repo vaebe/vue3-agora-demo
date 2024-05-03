a1555d4b7df65b44bb008275dce12745042d80e8
vaebe <18137693952@163.com>
2024-05-03 22:13:53 +0800

    feat: 音视频一对一

--- src/views/basicVideoCall/index.vue
+++ src/views/basicVideoCall/index.vue
@@ -16,6 +16,7 @@ async function onPublished(user: IAgoraRTCRemoteUser, mediaType: 'video' | 'audi
 
   await client!.subscribe(user, mediaType)
 
+  // 获取用户发布的视频流
   if (mediaType === 'video') {
     const remoteVideoTrack = user.videoTrack
 
@@ -34,80 +35,67 @@ async function onPublished(user: IAgoraRTCRemoteUser, mediaType: 'video' | 'audi
 // 创建客户端事件监听
 async function onUnPublished(user: IAgoraRTCRemoteUser, mediaType: 'video' | 'audio') {
   await client!.unsubscribe(user, mediaType)
-  const userId = user.uid.toString()
-
-  if (userId === '9001')
-    ElMessage('老师已停止直播!')
-}
-
-function createClientListener(client: IAgoraRTCClient) {
-  client.on('user-published', onPublished)
-  client.on('user-unpublished', onUnPublished)
-}
-
-interface ClientJoin {
-  client: IAgoraRTCClient
-  uid: number
+  remoteVideoUserId.value = ''
 }
 
 const { getProjectConfig, getAgoraToken } = useProjectConfig()
 
-// 加入频道
-async function clientJoin(opts: ClientJoin) {
-  const { client, uid } = opts
-
-  const { appId, certificate } = getProjectConfig()
-  const appid = appId
-  const channel = '6001'
-  const token = await getAgoraToken({ uid, channel, appId, certificate })
-  await client.join(appid, channel, token, uid)
-}
-
-const uid = ref(0)
+const localUid = ref(1001)
 
 let client: IAgoraRTCClient | null = null
-
-interface CreateVideoOpts {
-  videoDomId: string
-}
 
 let videoTrack: ICameraVideoTrack | null = null
 let audioTrack: IMicrophoneAudioTrack | null = null
 
-async function createBasicVideoCall(opts: CreateVideoOpts) {
-  if (uid.value < 1) {
-    ElMessage.warning('请输入 uid 且不可为 0')
-    return
-  }
-
+async function createBasicVideoCall() {
   // 创建客户端
   client = createClient({ mode: 'rtc', codec: 'vp8' })
 
   // 创建连接后需要立即开启监听,否则会无法正确监听到已加入频道的用户
-  createClientListener(client)
+  client.on('user-published', onPublished)
+  client.on('user-unpublished', onUnPublished)
 
   // 加入频道
-  await clientJoin({ client, uid: uid.value })
+  const { appId, certificate } = getProjectConfig()
+  const channel = '6001'
+  const token = await getAgoraToken({ uid: localUid.value, channel, appId, certificate })
+  await client.join(appId, channel, token, localUid.value)
 
   // 创建本地音频和视频轨道并发布
   videoTrack = await createCameraVideoTrack()
   audioTrack = await createMicrophoneAudioTrack()
-  videoTrack.play(opts.videoDomId)
+
+  // 指定 video 容器
+  videoTrack.play(String(localUid.value))
+  // 发布视频流可以被房间内其他用户监听 user-published 获取流展示
   await client.publish([videoTrack, audioTrack])
 }
 
+function init() {
+  createBasicVideoCall().then(() => {
+    // 加入房间时默认开启视频及音频,如不需要可在此处理
+    isCameraOn.value = true
+    isMicrophoneOn.value = true
+  })
+}
+
+// =======================本地音视频开关=====================================
 const isCameraOn = ref(true)
 const isMicrophoneOn = ref(true)
 
+// 视频
 async function toggleCamera() {
+  // 判断视频是否是开启的状态,是的话就关闭否则开启
   if (!isCameraOn.value)
     await videoTrack?.setMuted(false)
   else
     await videoTrack?.setMuted(true)
 
+  // 取反赋值
   isCameraOn.value = !isCameraOn.value
 }
 
+// 音频
 async function toggleMicrophone() {
   if (!isMicrophoneOn.value)
     await audioTrack?.setMuted(false)
@@ -117,6 +105,7 @@ async function toggleMicrophone() {
   isMicrophoneOn.value = !isMicrophoneOn.value
 }
 
+// 离开频道时关闭推流断开客户端连接
 function leave() {
   videoTrack && videoTrack.close()
   videoTrack = null
@@ -131,13 +120,6 @@ function leave() {
   client = null
 }
 
-function init() {
-  createBasicVideoCall({ videoDomId: 'local-video' }).then(res => {
-    isCameraOn.value = true
-    isMicrophoneOn.value = true
-  })
-}
-
 onBeforeUnmount(() => {
   leave()
 })
@@ -145,17 +127,19 @@ onBeforeUnmount(() => {
 
 <template>
   <el-card>
+    <p class="text-xl">频道固定为: 6001</p>
+
     <div class="flex justify-between flex-wrap">
       <div class="w-[482px]">
-        <p class="mb-2 text-xl">远程画面: {{ remoteVideoUserId }}</p>
+        <p class="mb-2">远程画面: {{ remoteVideoUserId }}</p>
         <video id="remote-video" class="w-full h-[241px] bg-gray"></video>
       </div>
       <div class="w-[482px]">
-        <p class="mb-2 text-xl">本地画面: {{ uid }}</p>
-        <video id="local-video" class="w-full h-[241px] bg-gray"></video>
+        <p class="mb-2">本地画面: {{ localUid }}</p>
+        <video :id="localUid" class="w-full h-[241px] bg-gray"></video>
 
         <div class="content-operation">
-          <el-input v-model.number="uid" placeholder="请输入uid" clearable></el-input>
+          <el-input v-model.number="localUid" placeholder="请输入uid" clearable></el-input>
           <el-button size="small" @click="init">
             初始化并加入频道
           </el-button>
